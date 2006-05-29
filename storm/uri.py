47e3edbda64fb0c16a814e9f6f56a4c141474a6f
Gustavo Niemeyer <gustavo@niemeyer.net>
2006-05-29 16:06:01 -0300

    Implemented basic uri module.

---
+++ storm/uri.py
@@ -0,0 +1,73 @@
+
+
+class URIError(Exception):
+    pass
+
+
+class URI(object):
+
+    username = None
+    password = None
+    host = None
+    port = None
+    database = None
+
+    def __init__(self, scheme):
+        self.scheme = scheme
+        self.options = {}
+
+    @classmethod
+    def parse(cls, uri_str):
+        try:
+            scheme, rest = uri_str.split(":", 1)
+        except ValueError:
+            raise URIError("URI has no scheme")
+        uri = cls(scheme)
+        if "?" in rest:
+            rest, options = rest.split("?", 1)
+            for pair in options.split("&"):
+                key, value = pair.split("=", 1)
+                uri.options[unescape(key)] = unescape(value)
+        if rest:
+            if not rest.startswith("//"):
+                uri.database = unescape(rest)
+            else:
+                rest = rest[2:]
+                if "/" in rest:
+                    rest, database = rest.split("/", 1)
+                    uri.database = unescape(database)
+                if "@" in rest:
+                    userpass, hostport = rest.split("@", 1)
+                else:
+                    userpass = None
+                    hostport = rest
+                if hostport:
+                    if ":" in hostport:
+                        host, port = hostport.rsplit(":", 1)
+                        uri.host = unescape(host)
+                        if port:
+                            uri.port = int(port)
+                    else:
+                        uri.host = unescape(hostport)
+                if userpass is not None:
+                    if ":" in userpass:
+                        username, password = userpass.rsplit(":", 1)
+                        uri.username = unescape(username)
+                        uri.password = unescape(password)
+                    else:
+                        uri.username = unescape(userpass)
+        return uri
+
+def unescape(s):
+    if "%" not in s:
+        return s
+    i = 0
+    j = s.find("%")
+    r = []
+    while j != -1:
+        r.append(s[i:j])
+        i = j+3
+        r.append(chr(int(s[j+1:i], 16)))
+        j = s.find("%", i)
+    r.append(s[i:])
+    return "".join(r)
---
+++ tests/uri.py
@@ -0,0 +1,113 @@
+from storm.uri import URI, URIError
+
+from tests.helper import TestHelper
+
+
+class URITest(TestHelper):
+
+    def test_constructor(self):
+        uri = URI("scheme")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.options, {})
+        self.assertEquals(uri.username, None)
+        self.assertEquals(uri.password, None)
+        self.assertEquals(uri.host, None)
+        self.assertEquals(uri.port, None)
+        self.assertEquals(uri.database, None)
+
+    def test_parse_no_colon(self):
+        self.assertRaises(URIError, URI.parse, "scheme")
+
+    def test_parse_just_colon(self):
+        uri = URI.parse("scheme:")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.database, None)
+
+
+    def test_parse_just_relative_database(self):
+        uri = URI.parse("scheme:d%61ta/base")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.database, "data/base")
+
+    def test_parse_just_absolute_database(self):
+        uri = URI.parse("scheme:/d%61ta/base")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.database, "/data/base")
+
+    def test_parse_host(self):
+        uri = URI.parse("scheme://ho%73t")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.host, "host")
+
+    def test_parse_username(self):
+        uri = URI.parse("scheme://user%6eame@")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.host, None)
+
+    def test_parse_username_password(self):
+        uri = URI.parse("scheme://user%6eame:pass%77ord@")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.password, "password")
+        self.assertEquals(uri.host, None)
+
+    def test_parse_username_host(self):
+        uri = URI.parse("scheme://user%6eame@ho%73t")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.host, "host")
+
+    def test_parse_username_password_host(self):
+        uri = URI.parse("scheme://user%6eame:pass%77ord@ho%73t")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.password, "password")
+        self.assertEquals(uri.host, "host")
+
+    def test_parse_username_password_host_port(self):
+        uri = URI.parse("scheme://user%6eame:pass%77ord@ho%73t:1234")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.password, "password")
+        self.assertEquals(uri.host, "host")
+        self.assertEquals(uri.port, 1234)
+
+    def test_parse_username_password_host_empty_port(self):
+        uri = URI.parse("scheme://user%6eame:pass%77ord@ho%73t:")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.password, "password")
+        self.assertEquals(uri.host, "host")
+        self.assertEquals(uri.port, None)
+
+    def test_parse_username_password_host_port_database(self):
+        uri = URI.parse("scheme://user%6eame:pass%77ord@ho%73t:1234/d%61tabase")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.username, "username")
+        self.assertEquals(uri.password, "password")
+        self.assertEquals(uri.host, "host")
+        self.assertEquals(uri.port, 1234)
+        self.assertEquals(uri.database, "database")
+
+    def test_parse_options(self):
+        uri = URI.parse("scheme:?a%62c=d%65f&ghi=jkl")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.host, None)
+        self.assertEquals(uri.database, None)
+        self.assertEquals(uri.options, {"abc": "def", "ghi": "jkl"})
+
+    def test_parse_host_options(self):
+        uri = URI.parse("scheme://ho%73t?a%62c=d%65f&ghi=jkl")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.host, "host")
+        self.assertEquals(uri.database, None)
+        self.assertEquals(uri.options, {"abc": "def", "ghi": "jkl"})
+
+    def test_parse_host_database_options(self):
+        uri = URI.parse("scheme://ho%73t/d%61tabase?a%62c=d%65f&ghi=jkl")
+        self.assertEquals(uri.scheme, "scheme")
+        self.assertEquals(uri.host, "host")
+        self.assertEquals(uri.database, "database")
+        self.assertEquals(uri.options, {"abc": "def", "ghi": "jkl"})
+
