37010b37781f0b0bc5accc1b9ef809829a4fb854
Jamshed Kakar <jkakar@pedro>
2006-12-04 19:32:57 -0800

    - Implemented EmptyResultSet.   - Implemented tests for SQLite, MySQL and PostgreSQL. - Updated MySQL table creation scripts to use ENGINE instead of TYPE;   TYPE is deprecated.

--- tests/store/mysql.py
+++ tests/store/mysql.py
@@ -2,7 +2,7 @@ import os
 
 from storm.database import create_database
 
-from tests.store.base import StoreTest
+from tests.store.base import StoreTest, EmptyResultSetTest
 from tests.helper import TestHelper
 
 
@@ -27,16 +27,38 @@ class MySQLStoreTest(TestHelper, StoreTest):
         connection.execute("CREATE TABLE foo "
                            "(id INT PRIMARY KEY AUTO_INCREMENT,"
                            " title VARCHAR(50) DEFAULT 'Default Title') "
-                           "TYPE=InnoDB")
+                           "ENGINE=InnoDB")
         connection.execute("CREATE TABLE bar "
                            "(id INT PRIMARY KEY AUTO_INCREMENT,"
                            " foo_id INTEGER, title VARCHAR(50)) "
-                           "TYPE=InnoDB")
+                           "ENGINE=InnoDB")
         connection.execute("CREATE TABLE bin "
                            "(id INT PRIMARY KEY AUTO_INCREMENT,"
                            " bin BLOB) "
-                           "TYPE=InnoDB")
+                           "ENGINE=InnoDB")
         connection.execute("CREATE TABLE link "
                            "(foo_id INTEGER, bar_id INTEGER) "
-                           "TYPE=InnoDB")
+                           "ENGINE=InnoDB")
+        connection.commit()
+
+
+class MySQLEmptyResultSetTest(TestHelper, EmptyResultSetTest):
+
+    def setUp(self):
+        TestHelper.setUp(self)
+        EmptyResultSetTest.setUp(self)
+
+    def tearDown(self):
+        TestHelper.tearDown(self)
+        EmptyResultSetTest.tearDown(self)
+
+    def create_database(self):
+        self.database = create_database(os.environ["STORM_MYSQL_URI"])
+
+    def create_tables(self):
+        connection = self.database.connect()
+        connection.execute("CREATE TABLE foo "
+                           "(id INT PRIMARY KEY AUTO_INCREMENT,"
+                           " title VARCHAR(50) DEFAULT 'Default Title') "
+                           "ENGINE=InnoDB")
         connection.commit()
