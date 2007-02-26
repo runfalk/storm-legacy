7146ab1b17cbfee6c33175c51f0f153701fdbb79
Christopher Armstrong <radix@twistedmatrix.com>
2007-02-26 12:22:58 -0700

    MORE DOCUMENTATION

--- storm/base.py
+++ storm/base.py
@@ -5,5 +5,11 @@ __all__ = ["Storm"]
 
 
 class Storm(object):
+    """An optional base class for objects stored in a Storm Store.
+
+    It causes your subclasses to be associated with a Storm
+    PropertyRegistry. It is necessary to use this if you want to
+    specify References with strings.
+    """
     __metaclass__ = PropertyPublisherMeta
 
