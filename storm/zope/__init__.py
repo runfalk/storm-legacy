ff09955f33e24b78c9125d0f9963f79b31337a51
Jamu Kakar <jkakar@kakar.ca>
2007-07-17 11:42:15 -0700

    - Added a missing Zope conditional check to ensure that storm.zope can   be imported when Zope is unavailable. - Minor cosmetic changes to configure.zcml.

--- storm/zope/__init__.py
+++ storm/zope/__init__.py
@@ -1,12 +1,18 @@
-from zope.security.checker import NoProxy, BasicTypes, _available_by_default
+has_zope = True
+try:
+    from zope.security.checker import (
+        NoProxy, BasicTypes, _available_by_default)
+except ImportError:
+    has_zope = False
 
 from storm.info import ObjectInfo
 
 
-# The followig is required for storm.info.get_obj_info() to have
+# The following is required for storm.info.get_obj_info() to have
 # access to a proxied object which is already in the store (IOW, has
 # the object info set already).  With this, Storm is able to
 # gracefully handle situations when a proxied object is passed to a
 # Store.
-_available_by_default.append("__object_info")
-BasicTypes[ObjectInfo] = NoProxy
+if has_zope:
+    _available_by_default.append("__object_info")
+    BasicTypes[ObjectInfo] = NoProxy
