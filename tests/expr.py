d25fa9609143eb4494e560e386fbb1e39c3941f3
Gustavo Niemeyer <gustavo@niemeyer.net>
2008-01-25 16:09:28 -0200

    Added a test equivalent to the one Thomas Herve added for update, but using an insert statement.  It suffers from the same problem.

--- tests/expr.py
+++ tests/expr.py
@@ -694,6 +694,14 @@ class CompileTest(TestHelper):
                         'VALUES (elem2, elem1)'), statement)
         self.assertEquals(state.parameters, [])
 
+    def test_insert_with_columns_to_escape(self):
+        expr = Insert({Column("column 1", table1): elem1}, table2)
+        state = State()
+        statement = compile(expr, state)
+        self.assertEquals(statement,
+                          'INSERT INTO "table 2" ("column 1") VALUES (elem1)')
+        self.assertEquals(state.parameters, [])
+
     def test_insert_auto_table(self):
         expr = Insert({Column(column1, table1): elem1})
         state = State()
