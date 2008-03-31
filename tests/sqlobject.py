0e34beba3f84f06e4be6585c4fc18ec9dee8a8bb
James Henstridge <james@jamesh.id.au>
2008-03-31 15:41:07 +0800

    Add a test for prejoins on the result of selectBy() (currently fails).

--- tests/sqlobject.py
+++ tests/sqlobject.py
@@ -740,7 +740,7 @@ class SQLObjectTest(TestHelper):
         class Address(self.SQLObject):
             city = StringCol()
 
-        result = Person.select("name = 'John Doe'", prejoins=["address"])
+        result = Person.select("person.name = 'John Doe'", prejoins=["address"])
         person = result[0]
 
         # Remove the row behind its back.
@@ -758,7 +758,8 @@ class SQLObjectTest(TestHelper):
         class Address(self.SQLObject):
             city = StringCol()
 
-        person = Person.selectOne("name = 'John Doe'", prejoins=["address"])
+        person = Person.selectOne("person.name = 'John Doe'",
+                                  prejoins=["address"])
 
         # Remove the row behind its back.
         self.store.execute("DELETE FROM address")
@@ -775,8 +776,26 @@ class SQLObjectTest(TestHelper):
         class Address(self.SQLObject):
             city = StringCol()
 
-        person = Person.selectFirst("name = 'John Doe'", prejoins=["address"],
-                                    orderBy="name")
+        person = Person.selectFirst("person.name = 'John Doe'",
+                                    prejoins=["address"], orderBy="name")
+
+        # Remove the row behind Storm's back.
+        self.store.execute("DELETE FROM address")
+
+        # They were prefetched, so it should work even then.
+        self.assertEquals(person.address.city, "Sao Carlos")
+
+    def test_result_set_prejoin_by(self):
+        """Ensure that prejoins work with selectBy() queries."""
+
+        class Person(self.Person):
+            address = ForeignKey(foreignKey="Address", dbName="address_id")
+
+        class Address(self.SQLObject):
+            city = StringCol()
+
+        result = Person.selectBy(name="John Doe").prejoin(["address"])
+        person = result[0]
 
         # Remove the row behind Storm's back.
         self.store.execute("DELETE FROM address")
