430766ac79280ae70a0965b8f21306f37667dee7
James Henstridge <james@jamesh.id.au>
2008-04-15 18:11:58 +0800

    Remove distinct modifier from union result sets.

--- storm/sqlobject.py
+++ storm/sqlobject.py
@@ -553,19 +553,22 @@ class SQLObjectResultSet(object):
         result_set = self._without_prejoins()._result_set.union(
             otherSelect._without_prejoins()._result_set, all=unionAll)
         result_set.order_by() # Remove default order.
-        return self._copy(prepared_result_set=result_set, orderBy=orderBy)
+        return self._copy(
+            prepared_result_set=result_set, distinct=False, orderBy=orderBy)
 
     def except_(self, otherSelect, exceptAll=False, orderBy=None):
         result_set = self._without_prejoins()._result_set.difference(
             otherSelect._without_prejoins()._result_set, all=exceptAll)
         result_set.order_by() # Remove default order.
-        return self._copy(prepared_result_set=result_set, orderBy=orderBy)
+        return self._copy(
+            prepared_result_set=result_set, distinct=False, orderBy=orderBy)
 
     def intersect(self, otherSelect, intersectAll=False, orderBy=None):
         result_set = self._without_prejoins()._result_set.intersection(
             otherSelect._without_prejoins()._result_set, all=intersectAll)
         result_set.order_by() # Remove default order.
-        return self._copy(prepared_result_set=result_set, orderBy=orderBy)
+        return self._copy(
+            prepared_result_set=result_set, distinct=False, orderBy=orderBy)
 
     def prejoin(self, prejoins):
         return self._copy(prejoins=prejoins)
