462bf7db0d65950fb72cc331fdfeb8154f218702
James Henstridge <james@jamesh.id.au>
2009-06-19 15:36:51 +0800

    Rework ResultSet._aggregate() so my changes are less of a hack.

--- storm/store.py
+++ storm/store.py
@@ -1184,30 +1184,25 @@ class ResultSet(object):
         self._having = And(*expr)
         return self
 
-    def _aggregate(self, expr, column=None):
+    def _aggregate(self, aggregate_func, expr, column=None):
         if self._group_by is not Undef:
             raise FeatureError("Single aggregates aren't supported after a "
                                " GROUP BY clause ")
         dummy, default_tables = self._find_spec.get_columns_and_tables()
         if (self._select is Undef and not self._distinct and
             self._offset is Undef and self._limit is Undef):
-            select = Select(expr, self._where, self._tables, default_tables)
+            select = Select(aggregate_func(expr), self._where,
+                            self._tables, default_tables)
         else:
-            if isinstance(expr, Count):
-                if expr.column is Undef:
-                    alias_expr = Undef
-                else:
-                    alias_expr = Alias(expr.column, "_expr")
-                    expr.column = alias_expr
-            else:
-                assert len(expr.args) == 1
-                alias_expr = Alias(expr.args[0], "_expr")
-                expr.args = (alias_expr,)
             columns = self._find_spec.get_primary_keys_and_exprs()
-            if alias_expr is not Undef:
-                columns.append(alias_expr)
+            if expr is Undef:
+                aggregate = aggregate_func(expr)
+            else:
+                alias = Alias(expr, "_expr")
+                columns.append(alias)
+                aggregate = aggregate_func(alias)
             subquery = replace_columns(self._get_select(), columns)
-            select = Select(expr, tables=Alias(subquery, "_tmp"))
+            select = Select(aggregate, tables=Alias(subquery, "_tmp"))
         result = self._store._connection.execute(select)
         value = result.get_one()[0]
         variable_factory = getattr(column, "variable_factory", None)
@@ -1219,26 +1214,26 @@ class ResultSet(object):
 
     def count(self, expr=Undef, distinct=False):
         """Get the number of objects represented by this ResultSet."""
-        return int(self._aggregate(Count(expr, distinct)))
+        return int(self._aggregate(lambda expr: Count(expr, distinct), expr))
 
     def max(self, expr):
         """Get the highest value from an expression."""
-        return self._aggregate(Max(expr), expr)
+        return self._aggregate(Max, expr, expr)
 
     def min(self, expr):
         """Get the lowest value from an expression."""
-        return self._aggregate(Min(expr), expr)
+        return self._aggregate(Min, expr, expr)
 
     def avg(self, expr):
         """Get the average value from an expression."""
-        value = self._aggregate(Avg(expr))
+        value = self._aggregate(Avg, expr)
         if value is None:
             return value
         return float(value)
 
     def sum(self, expr):
         """Get the sum of all values in an expression."""
-        return self._aggregate(Sum(expr), expr)
+        return self._aggregate(Sum, expr, expr)
 
     def values(self, *columns):
         """Retrieve only the specified columns.
