dda05c3dcdb0c2618c00c7e76e0c216141f27c87
Free Ekanayaka <free.ekanayaka@canonical.com>
2010-08-17 14:50:52 +0200

    Add ZSchema class

--- storm/zope/schema.py
+++ storm/zope/schema.py
@@ -1,5 +1,7 @@
 import transaction
 
+from storm.schema import Schema
+
 
 class ZStormCommitter(object):
 
@@ -8,3 +10,11 @@ class ZStormCommitter(object):
 
     def rollback(self):
         transaction.abort()
+
+
+class ZSchema(Schema):
+
+    def __init__(self, creates, drops, deletes, patch_package):
+        committer = ZStormCommitter()
+        super(ZSchema, self).__init__(creates, drops, deletes, patch_package,
+                                      committer)
