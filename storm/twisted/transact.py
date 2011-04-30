0d0a09b28f049eec3c44e486e566386ccb34e680
Free Ekanayaka <free.ekanayaka@canonical.com>
2011-04-30 13:12:08 +0200

    Add transact module and tests

---
+++ storm/twisted/transact.py
@@ -0,0 +1,99 @@
+import time
+import random
+import transaction
+
+from functools import wraps
+
+from zope.component import getUtility
+
+from storm.zope.interfaces import IZStorm
+from storm.exceptions import IntegrityError, DisconnectionError
+
+from twisted.internet import reactor
+from twisted.internet.threads import deferToThreadPool
+
+
+class Transactor(object):
+    """Wrap code that needs to interact with the database.
+
+    This wrapper make sure that code interacting with the database is run
+    in a separate thread and that the associated transaction is aborted or
+    committed in the same thread.
+
+    @param threadpool: The C{ThreadPool} to get threads from.
+    @param _transaction: The C{TransactionManager} to use, for test cases only.
+
+    @ivar retries: Maximum number of retries upon retriable exceptions.
+    """
+    retries = 2
+
+    def __init__(self, threadpool, _transaction=None):
+        self._threadpool = threadpool
+        if _transaction is None:
+            _transaction = transaction
+        self._transaction = _transaction
+        self._retriable_errors = (DisconnectionError, IntegrityError)
+        try:
+            from psycopg2.extensions import TransactionRollbackError
+            self._retriable_errors += (TransactionRollbackError,)
+        except ImportError:
+            pass
+
+    def run(self, function, *args, **kwargs):
+        """Run C{function} in a thread.
+
+        The function is run in a thread the L{TransactionWrapper}, which
+        commits the transaction if the function runs successfully. If it
+        raises an exception the transaction is aborted.
+
+        @param function: The function to run.
+        @param args: Positional arguments to pass to C{function}.
+        @param kwargs: Keyword arguments to pass to C{function}.
+        @return: A C{Deferred} that will fire after the function has been run.
+        """
+        return deferToThreadPool(
+            reactor, self._threadpool, self._wrap, function, *args, **kwargs)
+
+    def _wrap(self, function, *args, **kwargs):
+        retries = 0
+        while True:
+            try:
+                result = function(*args, **kwargs)
+                self._transaction.commit()
+            except self._retriable_errors, error:
+                if isinstance(error, DisconnectionError):
+                    # If we got a disconnection, calling rollback may not be
+                    # enough because psycopg2 doesn't necessarily use the
+                    # connection, so we call a dummy query to be sure that all
+                    # the stores are correct.
+                    zstorm = getUtility(IZStorm)
+                    for name, store in zstorm.iterstores():
+                        try:
+                            store.execute("SELECT 1")
+                        except DisconnectionError:
+                            pass
+                self._transaction.abort()
+                if retries < self.retries:
+                    retries += 1
+                    time.sleep(random.uniform(1, 2 ** retries))
+                    continue
+                else:
+                    raise
+            except:
+                self._transaction.abort()
+                raise
+            else:
+                return result
+
+
+def transact(method):
+    """Decorate L{method} so that it is invoked via L{Transactor.run}.
+
+    @param method: The method to decorate.
+    @return: A decorated method.
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        return self.transactor.run(method, self, *args, **kwargs)
+    return wrapper
---
+++ tests/twisted/transact.py
@@ -0,0 +1,279 @@
+import time
+import random
+import transaction
+
+from twisted.trial.unittest import TestCase
+
+from tests.helper import TestHelper
+from tests.zope import has_zope
+from tests.twisted import (has_twisted, has_psycopg)
+
+if has_zope and has_twisted:
+    from zope.component import getUtility
+
+    from storm.zope.interfaces import IZStorm
+    from storm.exceptions import IntegrityError, DisconnectionError
+
+    from storm.twisted.transact import Transactor, transact
+    from storm.twisted.testing import FakeThreadPool
+
+if has_psycopg:
+    from psycopg2.extensions import TransactionRollbackError
+
+
+class TransactorTest(TestCase, TestHelper):
+
+    def is_supported(self):
+        return has_zope and has_twisted
+
+    def setUp(self):
+        TestCase.setUp(self)
+        TestHelper.setUp(self)
+        self.threadpool = FakeThreadPool()
+        self.transaction = self.mocker.mock()
+        self.transactor = Transactor(self.threadpool, self.transaction)
+        self.function = self.mocker.mock()
+
+    def test_run(self):
+        """
+        L{Transactor.run} executes a function in a thread, commits
+        the transaction and returns a deferred firing the function result.
+        """
+        self.mocker.order()
+        self.expect(self.function(1, arg=2)).result(3)
+        self.expect(self.transaction.commit())
+        self.mocker.replay()
+        deferred = self.transactor.run(self.function, 1, arg=2)
+        deferred.addCallback(self.assertEqual, 3)
+        return deferred
+
+    def test_run_with_function_failure(self):
+        """
+        If the given function raises an error, then L{Transactor.run}
+        aborts the transaction and re-raises the same error.
+        """
+        self.mocker.order()
+        self.expect(self.function()).throw(ZeroDivisionError())
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, ZeroDivisionError)
+        return deferred
+
+    def test_run_with_disconnection_error(self):
+        """
+        If the given function raises a L{DisconnectionError}, then a C{SELECT
+        1} will be executed in each registered store such that C{psycopg}
+        actually detects the disconnection.
+        """
+        self.transactor.retries = 0
+        self.mocker.order()
+        zstorm = self.mocker.mock()
+        store1 = self.mocker.mock()
+        store2 = self.mocker.mock()
+        gu = self.mocker.replace(getUtility)
+        self.expect(self.function()).throw(DisconnectionError())
+        self.expect(gu(IZStorm)).result(zstorm)
+        self.expect(zstorm.iterstores()).result(iter((("store1", store1),
+                                                      ("store2", store2))))
+        self.expect(store1.execute("SELECT 1"))
+        self.expect(store2.execute("SELECT 1"))
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, DisconnectionError)
+        return deferred
+
+    def test_run_with_disconnection_error_in_execute_is_ignored(self):
+        """
+        If the given function raises a L{DisconnectionError}, then a C{SELECT
+        1} will be executed in each registered store such that C{psycopg}
+        actually detects the disconnection. If another L{DisconnectionError}
+        happens during C{execute}, then it is ignored.
+        """
+        self.transactor.retries = 0
+        zstorm = self.mocker.mock()
+        store1 = self.mocker.mock()
+        store2 = self.mocker.mock()
+        gu = self.mocker.replace(getUtility)
+        self.mocker.order()
+        self.expect(self.function()).throw(DisconnectionError())
+        self.expect(gu(IZStorm)).result(zstorm)
+        self.expect(zstorm.iterstores()).result(iter((("store1", store1),
+                                                      ("store2", store2))))
+        self.expect(store1.execute("SELECT 1")).throw(DisconnectionError())
+        self.expect(store2.execute("SELECT 1"))
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, DisconnectionError)
+        return deferred
+
+    def test_run_with_commit_failure(self):
+        """
+        If the given function succeeds but the transaction fails to commit,
+        then L{Transactor.run} aborts the transaction and re-raises
+        the commit exception.
+        """
+        self.mocker.order()
+        self.expect(self.function())
+        self.expect(self.transaction.commit()).throw(ZeroDivisionError())
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, ZeroDivisionError)
+        return deferred
+
+    def test_wb_default_transaction(self):
+        """
+        By default L{Transact} uses the global transaction manager.
+        """
+        transactor = Transactor(self.threadpool)
+        self.assertIdentical(transaction, transactor._transaction)
+
+    def test_decorate(self):
+        """
+        A L{transact} decorator can be used with methods of an object that
+        contains a L{Transactor} instance as a C{transactor} instance variable,
+        ensuring that the decorated function is called via L{Transactor.run}.
+        """
+        self.mocker.order()
+        self.expect(self.transaction.commit())
+        self.mocker.replay()
+
+        @transact
+        def function(self):
+            """docstring"""
+            return "result"
+
+        # Function metadata is copied to the wrapper.
+        self.assertEqual("docstring", function.__doc__)
+        deferred = function(self)
+        deferred.addCallback(self.assertEqual, "result")
+        return deferred
+
+    def test_run_with_integrity_error_retries(self):
+        """
+        If the given function raises a L{IntegrityError}, then the function
+        will be retried another two times before letting the exception bubble
+        up.
+        """
+        sleep = self.mocker.replace(time.sleep)
+        uniform = self.mocker.replace(random).uniform
+        self.mocker.order()
+
+        self.expect(self.function()).throw(IntegrityError())
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 1)).result(1)
+        self.expect(sleep(1))
+
+        self.expect(self.function()).throw(IntegrityError())
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 2)).result(2)
+        self.expect(sleep(2))
+
+        self.expect(self.function()).throw(IntegrityError())
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, IntegrityError)
+        return deferred
+
+    def test_run_with_transaction_rollback_error_retries(self):
+        """
+        If the given function raises a L{TransactionRollbackError}, then the
+        function will be retried another two times before letting the exception
+        bubble up.
+        """
+        if not has_psycopg:
+            return
+
+        sleep = self.mocker.replace(time.sleep)
+        uniform = self.mocker.replace(random).uniform
+        self.mocker.order()
+
+        self.expect(self.function()).throw(TransactionRollbackError())
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 1)).result(1)
+        self.expect(sleep(1))
+
+        self.expect(self.function()).throw(TransactionRollbackError())
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 2)).result(2)
+        self.expect(sleep(2))
+
+        self.expect(self.function()).throw(TransactionRollbackError())
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, TransactionRollbackError)
+        return deferred
+
+    def test_run_with_disconnection_error_retries(self):
+        """
+        If the given function raises a L{DisconnectionError}, then the
+        function will be retried another two times before letting the exception
+        bubble up.
+        """
+        zstorm = self.mocker.mock()
+        gu = self.mocker.replace(getUtility)
+        sleep = self.mocker.replace(time.sleep)
+        uniform = self.mocker.replace(random).uniform
+        self.mocker.order()
+
+        self.expect(self.function()).throw(DisconnectionError())
+        self.expect(gu(IZStorm)).result(zstorm)
+        self.expect(zstorm.iterstores()).result(iter(()))
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 1)).result(1)
+        self.expect(sleep(1))
+
+        self.expect(self.function()).throw(DisconnectionError())
+        self.expect(gu(IZStorm)).result(zstorm)
+        self.expect(zstorm.iterstores()).result(iter(()))
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 2)).result(2)
+        self.expect(sleep(2))
+
+        self.expect(self.function()).throw(DisconnectionError())
+        self.expect(gu(IZStorm)).result(zstorm)
+        self.expect(zstorm.iterstores()).result(iter(()))
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, DisconnectionError)
+        return deferred
+
+    def test_run_with_integrity_error_on_commit_retries(self):
+        """
+        If the given function raises a L{IntegrityError}, then the function
+        will be retried another two times before letting the exception bubble
+        up.
+        """
+        sleep = self.mocker.replace(time.sleep)
+        uniform = self.mocker.replace(random).uniform
+        self.mocker.order()
+
+        self.expect(self.function())
+        self.expect(self.transaction.commit()).throw(IntegrityError())
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 1)).result(1)
+        self.expect(sleep(1))
+
+        self.expect(self.function())
+        self.expect(self.transaction.commit()).throw(IntegrityError())
+        self.expect(self.transaction.abort())
+        self.expect(uniform(1, 2 ** 2)).result(2)
+        self.expect(sleep(2))
+
+        self.expect(self.function())
+        self.expect(self.transaction.commit()).throw(IntegrityError())
+        self.expect(self.transaction.abort())
+        self.mocker.replay()
+
+        deferred = self.transactor.run(self.function)
+        self.assertFailure(deferred, IntegrityError)
+        return deferred
