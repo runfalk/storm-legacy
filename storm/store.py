8963d89c0fc303194b264f7b93d9612c862fdbc3
Christopher Armstrong <radix@twistedmatrix.com>
2012-12-14 11:17:40 -0600

    try a different strategy for avoiding the error.

--- storm/store.py
+++ storm/store.py
@@ -1244,7 +1244,11 @@ class ResultSet(object):
                 alias = Alias(expr, "_expr")
                 columns.append(alias)
                 aggregate = aggregate_func(alias)
-            subquery = replace_columns(self._get_select(), columns)
+            # Ordering probably doesn't matter for any aggregates, and since
+            # replace_columns() blows up on an ordered query, we'll drop it.
+            select = self._get_select()
+            select.order_by = Undef
+            subquery = replace_columns(select, columns)
             select = Select(aggregate, tables=Alias(subquery, "_tmp"))
         result = self._store._connection.execute(select)
         value = result.get_one()[0]
@@ -1786,6 +1790,14 @@ def replace_columns(expr, columns):
             select.order_by = Undef
         return select
     elif isinstance(expr, SetExpr):
+        # The ORDER BY clause might refer to columns we have replaced.
+        # Luckily we can ignore it if there is no limit/offset.
+        if expr.order_by is not Undef and (
+            expr.limit is not Undef or expr.offset is not Undef):
+            raise FeatureError(
+                "__contains__() does not yet support set "
+                "expressions that combine ORDER BY with "
+                "LIMIT/OFFSET")
         subexprs = [replace_columns(subexpr, columns)
                     for subexpr in expr.exprs]
         return expr.__class__(
